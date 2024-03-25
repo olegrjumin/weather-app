ecaec2bd291f9d13c21a4ab7f2eb1aea3d83143a
Oleg Rjumin <oleg.rjumin@gmail.com>
2024-03-25 19:16:57 +0200

    Add cross fetch for tests

--- jest.config.js
+++ jest.config.js
@@ -9,6 +9,9 @@ const createJestConfig = nextJest({
 const customJestConfig = {
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   testEnvironment: "jsdom",
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": ["babel-jest", { presets: ["next/babel"] }],
+  },
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
