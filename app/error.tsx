7f89ef18564a0ee8ce72a2e4847944a03a12be43
Oleg Rjumin <oleg.rjumin@gmail.com>
2024-03-26 19:46:30 +0200

    Remove redundant handler

--- app/error.tsx
+++ app/error.tsx
@@ -19,7 +19,7 @@ export default function Error({
       <div>{error.message}</div>
       <button
         className="mt-4 rounded-md bg-blue-500 px-4 py-2 text-sm text-white transition-colors hover:bg-blue-400"
-        onClick={() => reset()}
+        onClick={reset}
       >
         Try again
       </button>
